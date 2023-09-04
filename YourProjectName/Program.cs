8dfe7585657ec9cb853c5d3d2ab6880c956aae9a
Dmytro Filinskyi <dfili@softserveinc.com>
2023-09-04 10:26:53 +0300

    UC#1 Create endpoint

--- YourProjectName/Program.cs
+++ YourProjectName/Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -13,13 +16,36 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.MapGet("/api/countries", async (string? str1, int? num1, string? str2, int? num2) =>
+{
+    using var httpClient = new HttpClient();
+    var response = await httpClient.GetAsync("https://restcountries.com/v3.1/all");
 
-app.MapControllers();
+    if (!response.IsSuccessStatusCode)
+    {
+        return Results.BadRequest("Could not retrieve countries");
+    }
+
+    var countries = await response.Content.ReadFromJsonAsync<List<Country>>();
+
+    // Here, you can use str1, num1, str2, num2 to filter or manipulate the countries list
+    // ...
+
+    return Results.Ok(countries);
+}).Produces<List<Country>>(); // Adding type information for Swagger
+
 
 app.Run();
+
+public class Country
+{
+    public string Name { get; set; }
+    public string Alpha2Code { get; set; }
+    public string Alpha3Code { get; set; }
+    public IEnumerable<string> Borders { get; set; }
+}
